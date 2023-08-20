a9bbe8e52fd7093e8e45e68b5b837ac85896ebbd
Alex Spaeth <atspaeth@ucsc.edu>
2023-08-19 17:58:25 -0700

    feat(exploratory): answers to Tal's questions
    StateTraversal now also includes box plots of the average entropy of the
    state distribution, weighted mainly towards the burst peak (but results
    are similar if all time points are weighted evenly).
    
    There is also a plot of the number of distinct trajectories, but since
    there is no statistically significant difference, this is less obviously
    useful, just something Tal was curious about.

--- StateTraversal.py
+++ StateTraversal.py
@@ -1,3 +1,4 @@
+import math
 import itertools
 
 import matplotlib.pyplot as plt
@@ -38,20 +39,19 @@ rasters = {
 # %%
 
 
-def list_of_states_traversed(exp):
+def states_traversed(exp):
     """
     For each model for the given experiment, return the average number of
     distinct states traversed per second in the scaffold window.
     """
     start, stop = srms[exp]["scaf_window"].ravel()
 
-    def rd(x):
-        return int(round(x / 30))
-
     for model in models[exp]:
+        T = model.bin_size_ms
         h = model.states(rasters[exp])
+        length = math.ceil((stop - start) / T)
         yield [
-            h[rd(peak + start) : rd(peak + stop) + 1]
+            h[(bin0 := int((peak + start) / T)) : bin0 + length]
             for peak in srms[exp]["tburst"].ravel()
         ]
 
@@ -63,7 +63,7 @@ def distinct_states_traversed(exp):
     """
     return [
         1e3 * np.mean([len(set(states)) / len(states) for states in model_states])
-        for model_states in list_of_states_traversed(exp)
+        for model_states in states_traversed(exp)
     ]
 
 
@@ -83,3 +83,79 @@ for a, b in itertools.combinations(subsets.keys(), 2):
         print(f"{a} vs. {b} is significant at ks = {stat:.2}, p = {100*p:.1e}% < 0.1%")
     else:
         print(f"{a} vs. {b} is insignificant ({p = :.1%})")
+
+# %%
+
+
+def entropy_of_states_traversed(exp):
+    for model, stateses in zip(models[exp], states_traversed(exp)):
+        dist = np.zeros((len(stateses[0]), model.n_states))
+        for states in stateses:
+            dist[np.arange(len(states)), states - 1] += 1 / len(stateses)
+        yield stats.entropy(dist, axis=1)
+
+
+def average_entropy(exp):
+    start, stop = srms[exp]["scaf_window"].ravel()
+    for entropies in entropy_of_states_traversed(exp):
+        # Create weights which peak at zero and fall off to the left and
+        # right.
+        zero = round(len(entropies) * -start / (stop - start))
+        weights = np.hstack(
+            [
+                np.linspace(0, 1, zero + 1, endpoint=False)[1:],
+                np.linspace(1, 0, len(entropies) - zero, endpoint=False),
+            ]
+        )
+        yield np.average(entropies, weights=weights)
+
+
+entropies = {
+    k: np.hstack([list(average_entropy(exp))
+                  for exp in tqdm(exps, desc=k)])
+    for k, exps in subsets.items()
+}
+
+plt.boxplot(entropies.values(), labels=traversed.keys())
+plt.ylabel("Average Entropy of State Distribution Throughout Bursts")
+
+
+for a, b in itertools.combinations(subsets.keys(), 2):
+    ks = stats.ks_2samp(entropies[a], entropies[b])
+    stat = ks.statistic
+    if (p := ks.pvalue) < 1e-3:
+        print(f"{a} vs. {b} is significant at ks = {stat:.2}, p = {100*p:.1e}%")
+    else:
+        print(f"{a} vs. {b} is insignificant (ks = {stat:.2}, {p = :.1%})")
+
+
+
+# %%
+
+def number_of_patterns(exp):
+    '''
+    Literally count how many distinct sequences of states are observed in each
+    of the models for the given experiment.
+    '''
+    for model, stateses in zip(models[exp], states_traversed(exp)):
+        yield len(set(tuple(states) for states in stateses))
+
+
+counts = {
+    k: np.hstack([list(number_of_patterns(exp))
+                    for exp in tqdm(exps, desc=k)])
+    for k, exps in subsets.items()
+}
+
+
+plt.boxplot(counts.values(), labels=counts.keys())
+plt.ylabel("Number of Distinct Trajectories Observed")
+
+
+for a, b in itertools.combinations(subsets.keys(), 2):
+    ks = stats.ks_2samp(counts[a], counts[b])
+    stat = ks.statistic
+    if (p := ks.pvalue) < 1e-3:
+        print(f"{a} vs. {b} is significant at ks = {stat:.2}, p = {100*p:.1e}%")
+    else:
+        print(f"{a} vs. {b} is insignificant (ks = {stat:.2}, {p = :.1%})")
