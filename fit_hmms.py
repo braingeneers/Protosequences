bca47f9cec738a236a69d496adf68c6e4702bd78
Alex Spaeth <atspaeth@ucsc.edu>
2023-09-05 14:36:14 -0700

    feat(nrp): allow clearing the queue

--- fit_hmms.py
+++ fit_hmms.py
@@ -9,6 +9,7 @@ import sys
 import itertools
 import numpy as np
 import hmmsupport
+import argparse
 from braingeneers.iot.messaging import MessageBroker
 
 
@@ -33,54 +34,50 @@ def parse_range_str(range_str):
         return np.array([int(range_str)])
 
 
-if __name__ == "__main__":
-    dryrun = "--dryrun" in sys.argv
-    if dryrun:
-        sys.argv.remove("--dryrun")
+def hmm_method_type(name):
+    if name not in hmmsupport._HMM_METHODS:
+        raise ValueError
+    return name
 
-    local = "--local" in sys.argv
-    if local:
-        sys.argv.remove("--local")
-    elif not os.environ.get("S3_USER"):
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            prog="fit_hmms",
+            description="Fit HMMs locally or on NRP.")
+    parser.add_argument('source')
+    parser.add_argument('exp',
+            type=lambda x: x if x == "*" else ensure_list(x))
+    parser.add_argument('bin_sizes', type=parse_range_str)
+    parser.add_argument('n_stateses', type=parse_range_str)
+    parser.add_argument('surrs', default=["real"], type=ensure_list)
+    parser.add_argument('library', default="default", nargs='?',
+            type=hmm_method_type)
+    parser.add_argument('-n', '--dryrun', action='store_true')
+    parser.add_argument('-l', '--local', action='store_true')
+    parser.add_argument('--clear-queue', action='store_true')
+    args = parser.parse_args()
+
+    if not args.local and not os.environ.get("S3_USER"):
         print("$S3_USER must be defined.", file=sys.stderr)
         sys.exit(1)
 
-    # Validate the arguments.
-    try:
-        if len(sys.argv) == 5:
-            sys.argv.append("real")
-        if len(sys.argv) == 6:
-            sys.argv.append("default")
-        _, source, exp, bin_size_str, nhs_str, surr, library = sys.argv
-
-        bin_sizes = parse_range_str(bin_size_str)
-        n_stateses = parse_range_str(nhs_str)
-
-    except Exception as e:
-        print("Invalid arguments.", e)
-        sys.exit(1)
-
-    if library not in hmmsupport._HMM_METHODS:
-        print(f"Invalid library `{library}`.")
-        sys.exit(1)
+    # Can't be part of the type because it depends on source.
+    if args.exp == "*":
+        args.exp = hmmsupport.all_experiments(args.source)
 
     # Verbosely print the full parameter set.
-    if exp == "*":
-        exps = hmmsupport.all_experiments(source)
-    else:
-        exps = ensure_list(exp)
-    surrs = ensure_list(surr)
-    print(f"Fitting HMMs using {library} for experiments:")
-    for exp, surr in itertools.product(exps, surrs):
-        print(f"  {source}/{exp}[{surr}]")
-    print("Will use K in", n_stateses)
-    print("Will use T in", bin_sizes)
+    print(f"Fitting HMMs using {args.library} for experiments:")
+    for exp, surr in itertools.product(args.exp, args.surrs):
+        print(f"  {args.source}/{exp}[{surr}]")
+    print("Will use K in", args.n_stateses)
+    print("Will use T in", args.bin_sizes)
 
     # Check which parameters actually need re-run.
     needs_run = [
         p
-        for p in itertools.product(exps, bin_sizes, n_stateses, surrs)
-        if not hmmsupport.is_cached(source, *p, library=library)
+        for p in itertools.product(args.exp, args.bin_sizes,
+            args.n_stateses, args.surrs)
+        if not hmmsupport.is_cached(args.source, *p, library=args.library)
     ]
     if not needs_run:
         print("All HMMs are already cached.")
@@ -88,33 +85,40 @@ if __name__ == "__main__":
 
     print(f"{len(needs_run)} HMMs need to be fit:")
     for exp, T, K, surrogate in needs_run:
-        print(f"  {source}/{exp}[{surrogate}] with {T=}ms, {K=}.")
+        print(f"  {args.source}/{exp}[{surrogate}] with {T=}ms, {K=}.")
 
     # If this is a dry run, don't actually bother queueing anything.
-    if dryrun:
+    if args.dryrun:
         sys.exit()
 
     # If local, don't use MQTT, just do all the fits in a loop here.
-    elif local:
+    elif args.local:
         for exp, bin_size_ms, n_states, surrogate in needs_run:
             hmmsupport.get_fitted_hmm(
-                source, exp, bin_size_ms, n_states, surrogate, library, verbose=True
+                args.source, exp, bin_size_ms, n_states, surrogate,
+                args.library, verbose=True
             )
+        sys.exit(0)
 
-    # Get the MQTT queue and add all the jobs to it.
-    else:
-        q = MessageBroker().get_queue("atspaeth/hmm-job-queue")
-        for exp, bin_size_ms, n_states, surrogate in needs_run:
-            q.put(
-                dict(
-                    retries_allowed=3,
-                    params=dict(
-                        source=source,
-                        exp=exp,
-                        bin_size_ms=bin_size_ms,
-                        n_states=n_states,
-                        surrogate=surrogate,
-                        library=library,
-                    ),
-                )
+    # Get the MQTT queue, clearing it if requested.
+    queue_name = f"{os.environ.get('S3_USER')}/hmm-job-queue"
+    mb = MessageBroker()
+    if args.clear_queue:
+        mb.delete_queue(queue_name)
+    q = mb.get_queue(queue_name)
+
+    # Add all the jobs to the queue.
+    for exp, bin_size_ms, n_states, surrogate in needs_run:
+        q.put(
+            dict(
+                retries_allowed=3,
+                params=dict(
+                    source=args.source,
+                    exp=exp,
+                    bin_size_ms=bin_size_ms,
+                    n_states=n_states,
+                    surrogate=surrogate,
+                    library=args.library,
+                ),
             )
+        )
