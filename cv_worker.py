d0f025eb41162d4cfa13567f9e952d31ddf77216
Alex Spaeth <atspaeth@ucsc.edu>
2023-09-13 17:09:51 -0700

    fix(surrogates): CV worker without model
    Directly construct the untrained model instead of relying on a trained
    model existing on S3.

--- cv_worker.py
+++ cv_worker.py
@@ -1,7 +1,8 @@
 import os
 import pickle
+from ssm import HMM
 from braingeneers.utils.smart_open_braingeneers import open
-from hmmsupport import get_raster, Model
+from hmmsupport import get_raster, _ssm_cv
 from worker import become_worker
 
 BASEPATH = f"s3://braingeneers/personal/{os.environ['S3_USER']}/cache/cv_scores"
@@ -9,11 +10,16 @@ BASEPATH = f"s3://braingeneers/personal/{os.environ['S3_USER']}/cache/cv_scores"
 
 def cross_validate(job):
     "Run cross-validation for a given parameter set."
-    model = Model(**job.params, verbose=True)
-    raster = get_raster(model.source, model.exp, model.bin_size_ms)
+    source = job.params["source"]
+    exp = job.params["exp"]
+    bin_size_ms = job.params["bin_size_ms"]
+    n_states = job.params["n_states"]
+    raster = get_raster(source, exp, bin_size_ms)
+    hmm = HMM(K=n_states, D=raster._raster.shape[1], observations="poisson")
     try:
-        training, validation, surrogate = model.cross_validate(raster, 5, verbose=True)
-        with open(f"{BASEPATH}/{model.tag}.pickle", "wb") as f:
+        training, validation, surrogate = _ssm_cv(hmm, raster, 5, verbose=True)
+        tag = f"{source}_{exp}_{bin_size_ms}ms_K{n_states}_real"
+        with open(f"{BASEPATH}/{tag}.pickle", "wb") as f:
             pickle.dump(
                 dict(validation=validation, training=training, surrogate=surrogate), f
             )
