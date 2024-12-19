be23585204fe51af1f5d5b25eefd4125794ce74d
Alex Spaeth <atspaeth@ucsc.edu>
2024-12-18 20:16:20 -0800

    Use glmer+Poisson instead of lmer for dims

--- HMM_analysis_python/lmem.R
+++ HMM_analysis_python/lmem.R
@@ -4,12 +4,18 @@ library('lmerTest')
 library('emmeans')
 
 dimensions <- read.csv("dimensions.csv")
+dsub = dimensions[dimensions$theta == 0.75, ]
+# Make a LMEM of this current subset of the data.
+sample_type <- factor(dsub$sample_type)
+sample_id <- factor(dsub$sample_id)
+model <- glmer(dsub$dims ~ sample_type + (1 | sample_id), family=poisson(link="log"))
+emmeans(model, pairwise ~ sample_type)
+
 df <- do.call(rbind, lapply(split(dimensions, dimensions$theta), function(dsub) {
 		# Make a LMEM of this current subset of the data.
 		sample_type <- factor(dsub$sample_type)
 		sample_id <- factor(dsub$sample_id)
-		dim_mi <- (dsub$dims - dsub$dims_rsm) / (dsub$dims + dsub$dims_rsm)
-		model <- lmer(dim_mi ~ sample_type + (1 | sample_id))
+		model <- glmer(dsub$dims ~ sample_type + (1 | sample_id), family=poisson(link="log"))
 		# Calculate the significance scores and add the theta column.
 		ret <- summary(emmeans(model, pairwise ~ sample_type)$contrasts)
 		ret$theta <- dsub$theta[[1]]
